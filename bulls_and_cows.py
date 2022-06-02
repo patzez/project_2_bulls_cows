d2e8efc63f6b143d78aca9c88b962344da2fcc3e
patzez <pat.zezulka@gmail.com>
2022-06-02 22:01:42 +0200

    Generate number + input number

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -9,21 +9,28 @@ discord: patzez#8128
 import random
 
 
-def check_start_number(number):
+def check_start_number(number: int) -> bool:
     if not str(number).startswith("0"):
         return True
     else:
         return False
 
 
-def check_duplicate(number):
+def check_duplicate(number: int) -> bool:
     if len(list(str(number))) == len(set(str(number))):
         return True
     else:
         return False
 
 
-def generate_number():
+def check_len(number: int) -> bool:
+    if len(str(number)) == 4:
+        return True
+    else:
+        return False
+
+
+def generate_number() -> int:
     while True:
         number = random.randint(1000, 9999)
         if check_start_number(number) and check_duplicate(number):
@@ -33,4 +40,17 @@ def generate_number():
     return number
 
 
+def input_number():
+    while True:
+        number = input("Enter a number: ")
+        if number.isdecimal() and check_duplicate(number) and check_len(number) and check_start_number(number):
+            break
+        else:
+            print("Invalid input.")
+            continue
+    return number
+
+
+vyber = input_number()
+print(vyber)
 print(generate_number())
