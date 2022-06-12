6cff1f7e42344c29468d401518a50a0ad97e173f
patzez <pat.zezulka@gmail.com>
2022-06-12 18:28:02 +0200

    the game

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -81,12 +81,56 @@ def count_bulls_cows(generated: int, guess: int) -> tuple:
     return bulls, cows
 
 
-vyber = input_number()
-generovane = generate_number()
-print(type(vyber),
-      num_to_list(vyber))
-print(type(generovane),
-      num_to_list(generovane))
-print(list(zip(num_to_list(vyber), num_to_list(generovane))))
-print(type(count_bulls_cows(generovane, vyber)),
-      count_bulls_cows(generovane, vyber))
+def num_of_tries() -> int:
+    while True:
+        tries = input("Enter number of tries: ")
+        if tries.isnumeric() and int(tries) > 0:
+            break
+        else:
+            print("Number of tries must be a whole number greater than 0!")
+            continue
+    return int(tries)
+
+
+def play_bulls_and_cows():
+    while True:
+        number = generate_number()
+        tries = num_of_tries()
+
+        while tries > 0:
+            guess = input_number()
+            b_and_c = count_bulls_cows(number, guess)
+            print(f"{b_and_c[0]} bulls, {b_and_c[0]} cows")
+
+            if b_and_c[0] == 4:
+                print("You guessed right!")
+                break
+
+            tries -= 1
+
+        again = input("Play again? [y / n]")
+
+        if again == "y":
+            continue
+        elif again == "n":
+            break
+        else:
+            print("Invalid imput!")
+
+
+
+
+
+
+
+
+
+# vyber = input_number()
+# generovane = generate_number()
+# print(type(vyber),
+#       num_to_list(vyber))
+# print(type(generovane),
+#       num_to_list(generovane))
+# print(list(zip(num_to_list(vyber), num_to_list(generovane))))
+# print(type(count_bulls_cows(generovane, vyber)),
+#       count_bulls_cows(generovane, vyber))
